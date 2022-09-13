f9e1d72fcea55267c2261dc1f51ed141fd12e54c
yl-1993 <yangleidev@gmail.com>
2022-09-13 19:57:06 +0800

    Remove unused html setting in conf.py

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -48,6 +48,7 @@ autodoc_mock_imports = [
     'filterpy'
 ]
 
+# Parse `Returns` in docstr with parameter style
 napoleon_custom_sections = [('Returns', 'params_style')]
 
 # Ignore >>> when copying code
@@ -77,12 +78,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-# html_css_files = ['css/readthedocs.css']
 
 # Enable ::: for my_st
 myst_enable_extensions = ['colon_fence']
