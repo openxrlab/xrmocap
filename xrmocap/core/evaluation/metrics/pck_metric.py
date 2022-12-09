43832bdc0a1a38ed4368f4c60ae8b1e785ebf5af
GaoYang <gaoyang3@sensetime.com>
2022-12-09 17:04:40 +0800

    add mpjpe and pampjpe

---
+++ xrmocap/core/evaluation/metrics/pck_metric.py
@@ -0,0 +1,48 @@
+# yapf: disable
+import logging
+import numpy as np
+from typing import Union
+
+from xrmocap.data_structure.keypoints import Keypoints
+from xrmocap.utils.eval_utils import (
+    align_by_keypoint, compute_similarity_transform,
+)
+from .base_metric import BaseMetric
+
+# yapf: enable
+
+
+class PCKMetric(BaseMetric):
+    RANK = 0
+
+    def __init__(
+        self,
+        name: str,
+        threshold: float,
+        logger: Union[None, str, logging.Logger] = None,
+    ) -> None:
+        BaseMetric.__init__(self, name=name, logger=logger)
+        self.threshold = threshold
+
+    def __call__(self, pred_keypoints3d: Keypoints, gt_keypoints3d: Keypoints,
+                 **kwargs):
+        pred_kps3d = pred_keypoints3d.get_keypoints()[..., :3]
+        gt_kps3d = gt_keypoints3d.get_keypoints()[..., :3]
+        _, _, rotation, scaling, transl = compute_similarity_transform(
+            gt_kps3d, pred_kps3d, compute_optimal_scale=True)
+        pred_kps3d_pa = (scaling * pred_kps3d.dot(rotation)) + transl
+
+        pred_keypoints3d_pa = pred_keypoints3d.clone()
+        pred_kps3d_conf = pred_keypoints3d.get_keypoints()[..., -1:]
+        pred_keypoints3d_pa.set_keypoints(
+            np.concatenate((pred_kps3d_pa, pred_kps3d_conf), axis=-1))
+        if self.align_kps_name is None:
+            pred_kps3d_pa = pred_keypoints3d_pa.get_keypoints()[..., :3]
+            gt_kps3d = gt_keypoints3d.get_keypoints()[..., :3]
+        else:
+            pred_kps3d_pa = align_by_keypoint(pred_keypoints3d_pa,
+                                              self.align_kps_name)
+            gt_kps3d = align_by_keypoint(gt_keypoints3d, self.align_kps_name)
+        pa_mpjpe_value = np.sqrt(
+            np.sum(np.square(pred_kps3d_pa - gt_kps3d), axis=-1))
+        return dict(pa_mpjpe_value=pa_mpjpe_value)
