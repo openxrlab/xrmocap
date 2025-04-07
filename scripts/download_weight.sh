847ad169737e30760ee5542842ec2743d0cc6cb3
LazyBusyYang <gaoyang_cn@outlook.com>
2025-04-07 14:22:18 +0800

    Update download links and install scripts for datasets and models (#173)
    - Changed dataset download links in documentation to Google Drive for easier access.
    - Updated installation scripts to use `gdown` for downloading files instead of `wget`.
    - Added missing image resource for SMPLify classes.
    - Installed `gdown` in the test data download script for consistency.
    
    These changes improve the accessibility and reliability of dataset and model downloads.

--- scripts/download_weight.sh
+++ scripts/download_weight.sh
@@ -2,7 +2,9 @@ mkdir -p weight/mvpose
 cd weight
 wget https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
 wget https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_wholebody_384x288_dark-f5726563_20200918.pth
-wget https://openxrlab-share-mainland.oss-cn-hangzhou.aliyuncs.com/xrmocap/weight/limb_info.json
+# limb_info.json
+gdown https://docs.google.com/uc?id=1FKzJVXno88xQj7MDyroFzm7ueofjsMH6
 cd mvpose
-wget https://openxrlab-share-mainland.oss-cn-hangzhou.aliyuncs.com/xrmocap/weight/resnet50_reid_camstyle-98d61e41_20220921.pth
+# resnet50_reid_camstyle
+gdown https://docs.google.com/uc?id=1HScJmiJ-18ioLXmUK_sBrPFZakZWwc4e
 cd ../..
